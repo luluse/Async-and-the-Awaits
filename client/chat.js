80f9819c4d2b93e57a1c7f0055a9cca0248da1ee
Alexander Whan <whan.alexander@gmail.com>
2020-09-05 17:37:58 -0700

    Changed connect link to Heroku app

--- client/chat.js
+++ client/chat.js
@@ -5,9 +5,9 @@ require('dotenv').config();
 const inquirer = require('inquirer');
 
 const io = require('socket.io-client');
-// const serverChannel = io.connect('http://localhost:3001/server');
+// const serverChannel = io.connect('http://localhost:3001');
 const serverChannel = io.connect(
-  'https://command-love-interface.herokuapp.com/server'
+  'https://command-love-interface.herokuapp.com'
 );
 
 serverChannel.emit('join', 'I just joined!');
