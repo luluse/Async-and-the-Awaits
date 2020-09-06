8a57954364e40aeb5741081c4fb50ff74b246516
Alexander Whan <whan.alexander@gmail.com>
2020-09-05 17:07:03 -0700

    Modified server to use HTTP

--- server/server.js
+++ server/server.js
@@ -1,25 +1,21 @@
 'use strict';
 
+const http = require('http'); // Built-in Node feature
 require('dotenv').config();
 const PORT = process.env.PORT;
 
-const io = require('socket.io')(3000); // what about (process.env.PORT || 3000)? Can it be same PORT for Socket.io/Express?
-const server = io.of('/server');
+// const server = io.of('/server');
 
 const express = require('express');
 const app = express();
+const server = http.createServer(app);
+const io = require('socket.io').listen(server); // what about (process.env.PORT || 3000)? Can it be same PORT for Socket.io/Express?
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT, () => {
-  console.log(`Listening on Express Server on ${PORT}`);
-});
-
 const messageQueue = {};
 
-// Express server needs to be listening before anything below this happens
-
-server.on('connection', socket => {
+io.on('connection', socket => {
   console.log('Connected on: ', PORT, socket.id);
 
   socket.on('join', room => {
@@ -29,10 +25,10 @@ server.on('connection', socket => {
 
   socket.on('message', messageFromClient => {
     console.log('Received: ', messageFromClient);
-    server.emit('received', messageFromClient);
+    io.emit('received', messageFromClient);
   });
 });
 
-// Make an express server
-// LISTEN on express server (app.listen) - needs to be BEFORE the io/server.on
-// AFTER that, do server.on in the code with socket.io
+app.listen(PORT, () => {
+  console.log(`Listening on Express Server on ${PORT}`);
+});
