882d476ede1fa49f7f560a43d69472bf736222cd
Alexander Whan <whan.alexander@gmail.com>
2020-09-07 11:35:04 -0700

    Added correct path to live server URI

--- client/chat.js
+++ client/chat.js
@@ -5,10 +5,10 @@ require('dotenv').config();
 const inquirer = require('inquirer');
 
 const io = require('socket.io-client');
-const serverChannel = io.connect('http://localhost:3001');
-// const serverChannel = io.connect(
-//   'https://command-love-interface.herokuapp.com'
-// );
+// const serverChannel = io.connect('http://localhost:3001');
+const serverChannel = io.connect(
+  'https://command-love-interface.herokuapp.com/server'
+);
 
 serverChannel.emit('join', 'I just joined!');
 
