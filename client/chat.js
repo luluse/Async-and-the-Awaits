c6a43c9319b6bd80ff417022987aef6a0d4105f1
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2020-09-08 15:26:52 -0700

    created a hard coded basic auth

--- client/chat.js
+++ client/chat.js
@@ -10,8 +10,6 @@ const serverChannel = io.connect(
   'https://command-love-interface.herokuapp.com'
 );
 
-
-
 serverChannel.emit('join', 'I just joined!');
 
 // Server should send the message back to the sender as confirmation (for testing purposes only until we get it working)
@@ -32,6 +30,34 @@ function sendMessage(text) {
   let message = `[${username}]: ${text}`;
   serverChannel.emit('message', message);
 }
+let firstUser = {
+  username: 'lulu',
+  // password: 'password',
+};
+
+async function loginOrCreate(){
+  // console.log('Please enter username');
+  // console.log('Please enter password');
+  let input = await inquirer.prompt([
+    { name: 'username', message: 'Please enter username *********' },
+  ]);
+  // sendMessage(input.text);
+  // getInputLogin();
+  if(input.username === firstUser.username){
+    username = input.username;
+    console.log(`Welcome to the chat, ${username}!`);
+    getInput();
+  } else {
+    console.log('wrong!');
+    loginOrCreate();
+  }
+}
+
+async function getInputLogin() {
+  // inquirer grabs input from the CLI
+  let input = await inquirer.prompt([{name: 'username', message: 'enter your username' }]);
+  sendMessage(input.username);
+}
 
 async function getInput() {
   // inquirer grabs input from the CLI
@@ -40,18 +66,18 @@ async function getInput() {
   getInput();
 }
 
-
 async function getName() {
-  console.clear();
+  // console.clear();
   let input = await inquirer.prompt([
-    { name: 'name', message: 'Please provide username and password.' },
+    { name: 'name', message: 'Please provide username' },
   ]);
-  let nameCheck = await inquirer.prompt([
-    { name: 'name', message: 'What do you want to' },
-  ]);
+  // let nameCheck = await inquirer.prompt([
+  //   { name: 'name', message: 'What do you want to' },
+  // ]);
 
   username = input.name;
 }
 
-getName();
-getInput(); // Calling these here works locally - but as far as can tell NOT the same way in Heroku
+loginOrCreate();
+// getName();
+// getInput();
