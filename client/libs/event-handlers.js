ee82576587a3ee38f89db1731006ba269a9c1403
Alexander Whan <whan.alexander@gmail.com>
2020-09-09 15:40:50 -0700

    Added menu landing page to serve as client index

--- client/libs/event-handlers.js
+++ client/libs/event-handlers.js
@@ -1,1 +1,15 @@
+/* eslint-disable comma-dangle */
 'use strict';
+
+require('dotenv').config();
+const inquirer = require('inquirer');
+const io = require('socket.io-client');
+
+const serverChannel = io.connect(
+  'https://command-love-interface.herokuapp.com'
+);
+// const serverChannel = io.connect('http://localhost:3001');
+
+module.exports = {
+  serverChannel,
+};
---
+++ client/menu.js
@@ -0,0 +1,5 @@
+'use strict';
+
+require('dotenv').config();
+const inquirer = require('inquirer');
+const io = require('socket.io-client');
