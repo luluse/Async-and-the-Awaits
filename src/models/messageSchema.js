087d6dac50a06372ed22c3e0faf69714baa307f4
Alexander Whan <whan.alexander@gmail.com>
2020-09-10 12:24:30 -0700

    Updated messageSchema with sender/room props

--- src/models/messageSchema.js
+++ src/models/messageSchema.js
@@ -3,8 +3,11 @@
 const mongoose = require('mongoose');
 
 let messageObject = {
-  message: { type: String},
+  message: { type: String, required: true },
+  sender: { type: String, required: true },
+  room: { type: String, default: 'lobby' },
 };
+
 const messages = new mongoose.Schema(messageObject);
 
 module.exports = mongoose.model('messages', messages);
